ade07a7a0b9c54e32ac961e6f261124dcbb5d4a7
ygblog <117955401+ning0818@users.noreply.github.com>
2023-10-30 15:13:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ ss.http({
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Vercel-CDN-Cache-Control', 'max-age=7200');
         res.setHeader('Cache-Control', 'max-age=7200');
-        fetch("https://gpt.haixuntech.cn/api/v1/conversation/RefreshChat", {
+        fetch("https://gpt.haixuntech.cn/api/v1/conversation/ChatV2", {
             method: "POST",
             headers: {
                 "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3MTkxOTAxNTMsImlhdCI6MTY5ODU1MDE1Mywiand0VXNlcklkIjoiNjUzZGQxODk3MzRhYmY5YTQ2ZWUwMjBhIn0.ZfGyz89Lj61tKx5TMHtvx3HhmPykj-ERT80-bFEJW-E",
@@ -18,7 +18,7 @@ ss.http({
                 "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 Edg/118.0.2088.69"
             },
             body: JSON.stringify({
-                messages: `[{"content":"","id":"LEMUR_AI_SYSTEM_SETTING","isSensitive":false,"needCheck":false,"role":"system"},{"content":"概述一下这篇文章：${ req.getQueryVariable("content", "") }","isSensitive":false,"needCheck":true,"role":"user"}]`
+                messages: `[{"conversation_id":"653f508b734abf9a46f06073","content":"概述一下这篇文章：${ req.getQueryVariable("content", "") }","chat_context_id_list":[],"is_gpt4":false,"model_name":"gpt-3.5-turbo","system_role_prompt":"Chat Ai对话","base_request":{"platform_type":"Web","client_version":"2.1","trace_id":"","signature":"","share":""}}]`
             })
         }).then(function (response) {
             response.text().then(function(t){
