e856f2e07b4b73fd4e4b10f1b78b20843c09db5e
ygblog <117955401+ning0818@users.noreply.github.com>
2023-11-06 20:31:38 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
+@@ -1,4 +1,4 @@
  import ss from "simplest-server";
 import fetch from "node-fetch";
 
 ss.http({
-    '/': function(req, res){
-        res.setHeader("Access-Control-Allow-Methods", "*");
-        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-        res.setHeader('Access-Control-Allow-Credentials', true);
+@ -9,35 +9,33 @@
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader('Vercel-CDN-Cache-Control', 'max-age=7200');
         res.setHeader('Cache-Control', 'max-age=7200');
-        fetch("https://wetabchat.haohuola.com/api/chat/conversation-v2", {
+        fetch("http://lemurchat.anfans.cn/api/chat/conversation-trial", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
                 "User-Agent": "Mozilla/5.0 (Linux; Android 9; Redmi 4 Prime) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Mobile Safari/537.36"
             },
-            body: JSON.stringify(`[{"prompt":"概述一下这篇文章：${ req.getQueryVariable("content", "") }","assistantId":"","model":"650e52e9c4bcb4a52791b599"}]`)
+            body: JSON.stringify({
+                messages: `[{"content":"","id":"LEMUR_AI_SYSTEM_SETTING","isSensitive":false,"needCheck":false,"role":"system"},{"content":"概述一下这篇文章：${ req.getQueryVariable("content", "") }","isSensitive":false,"needCheck":true,"role":"user"}]`
+            })
         }).then(function (response) {
             response.text().then(function(t){
                 var ls = t.split("\n\n")
@@ -37,3 +37,4 @@ ss.http({
             })
         });
     }}).listen(3000)
+    
